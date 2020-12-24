9a7042d50a6ed29536ef7bd0f1fe49ba72189034
David Jones <alexander.chaconas@gmail.com>
2020-12-24 12:41:06 -0600

    added tooltips to icons

--- js/app.js
+++ js/app.js
@@ -47,7 +47,10 @@ $("#projects").append($div)
 
 })
 
-
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
 
 
 
